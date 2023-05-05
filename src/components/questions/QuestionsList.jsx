a2079fd8a7a5f91000199306dae452a90644725b
EliasNegasa <eliasnegasa5@gmail.com>
2023-05-06 02:11:15 +0300

    answer

--- src/components/questions/QuestionsList.jsx
+++ src/components/questions/QuestionsList.jsx
@@ -5,17 +5,13 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import AlarmOutlinedIcon from "@mui/icons-material/AlarmOutlined";
-import { Grid, Stack } from "@mui/material";
+import { Chip, Grid, Stack } from "@mui/material";
 import StickyNote2OutlinedIcon from "@mui/icons-material/StickyNote2Outlined";
 import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
-import ParseDate from "../../utils/parseDate";
 import BackdropLoader from "../ui/Backdrop";
 import NotificationSnackbars from "../ui/Snackbar";
 import { useQuery } from "@tanstack/react-query";
 import Popup from "../ui/Popup";
-import QuizOutlinedIcon from "@mui/icons-material/QuizOutlined";
-import CircleOutlinedIcon from "@mui/icons-material/CircleOutlined";
 import RadioButtonCheckedOutlinedIcon from "@mui/icons-material/RadioButtonCheckedOutlined";
 import { getQuestions } from "../../services/questions";
 import CreateQuestion from "./CreateQuestion";
@@ -78,6 +74,17 @@ const QuestionsList = () => {
                   }}
                 >
                   <CardContent sx={{ flex: "1 0 auto" }}>
+                    <Chip
+                      label={
+                        question.exam
+                          ? question.exam.exam_name
+                          : "No exam assigned"
+                      }
+                      color={question.exam ? "success" : "error"}
+                      variant="outlined"
+                      size="small"
+                      sx={{ mb: "10px" }}
+                    />
                     <Typography component="div" variant="p">
                       {question.question_text}
                     </Typography>
@@ -92,24 +99,19 @@ const QuestionsList = () => {
                     sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}
                   >
                     <Stack direction="column">
-                      <Stack direction="row" sx={{ alignItems: "center" }}>
-                        <IconButton>
-                          <RadioButtonCheckedOutlinedIcon />
-                        </IconButton>
-                        answer
-                      </Stack>
-                      <Stack direction="row" sx={{ alignItems: "center" }}>
-                        <IconButton>
-                          <RadioButtonCheckedOutlinedIcon />
-                        </IconButton>
-                        answer
-                      </Stack>
-                      <Stack direction="row" sx={{ alignItems: "center" }}>
-                        <IconButton>
-                          <RadioButtonCheckedOutlinedIcon />
-                        </IconButton>
-                        answer
-                      </Stack>
+                      {question.answers &&
+                        question.answers.map((answer) => (
+                          <Stack
+                            key={answer.id}
+                            direction="row"
+                            sx={{ alignItems: "center" }}
+                          >
+                            <IconButton>
+                              <RadioButtonCheckedOutlinedIcon />
+                            </IconButton>
+                            {answer.answer_text}
+                          </Stack>
+                        ))}
                     </Stack>
                   </Box>
                   <Stack
@@ -118,9 +120,16 @@ const QuestionsList = () => {
                       justifyContent: "flex-end",
                       alignItems: "stretch",
                       width: "100%",
-                      margin: "0 auto",
+                      ml: "10px",
                     }}
                   >
+                    {/* <Button
+                      variant="text"
+                      // onClick={() => handleEditClicked(question)}
+                      startIcon={<ModeEditOutlineOutlinedIcon />}
+                    >
+                      Edit Answers
+                    </Button> */}
                     <Button
                       variant="text"
                       onClick={() => handleEditClicked(question)}
@@ -159,89 +168,3 @@ const QuestionsList = () => {
 };
 
 export default QuestionsList;
-
-const questionData = [
-  {
-    id: 2,
-    question_text:
-      "An angular 2 project written in typescript is* transpiled to javascript duri*ng the build process. Which of the following additional features are provided to the developer while programming on typescript over javascript?",
-    question_type: "choice",
-    remarks: null,
-    createdAt: "2023-05-05T21:56:44.822Z",
-    updatedAt: "2023-05-05T21:56:44.822Z",
-    exam_id: 1,
-    exam: {
-      id: 1,
-      exam_name: "Java Exam",
-      duration_minutes: 120,
-      exam_start: "2023-04-12T20:26:44.755Z",
-      exam_end: "2023-04-12T20:26:44.755Z",
-      createdAt: "2023-05-05T15:29:15.540Z",
-      updatedAt: "2023-05-05T15:29:15.540Z",
-      course_id: null,
-    },
-    answers: [],
-  },
-  {
-    id: 5,
-    question_text: "typescript over javascript?",
-    question_type: "choice",
-    remarks: null,
-    createdAt: "2023-05-05T21:58:06.401Z",
-    updatedAt: "2023-05-05T21:58:06.401Z",
-    exam_id: null,
-    exam: null,
-    answers: [],
-  },
-  {
-    id: 4,
-    question_text: "Which of the following additional features are provided",
-    question_type: "choice",
-    remarks: null,
-    createdAt: "2023-05-05T21:57:42.605Z",
-    updatedAt: "2023-05-05T21:57:42.605Z",
-    exam_id: null,
-    exam: null,
-    answers: [],
-  },
-  {
-    id: 1,
-    question_text: "Package?",
-    question_type: "choice",
-    remarks: null,
-    createdAt: "2023-05-05T21:56:03.874Z",
-    updatedAt: "2023-05-05T21:56:03.874Z",
-    exam_id: 1,
-    exam: {
-      id: 1,
-      exam_name: "Java Exam",
-      duration_minutes: 120,
-      exam_start: "2023-04-12T20:26:44.755Z",
-      exam_end: "2023-04-12T20:26:44.755Z",
-      createdAt: "2023-05-05T15:29:15.540Z",
-      updatedAt: "2023-05-05T15:29:15.540Z",
-      course_id: null,
-    },
-    answers: [],
-  },
-  {
-    id: 3,
-    question_text: "An angular 2 project written in typescript",
-    question_type: "choice",
-    remarks: null,
-    createdAt: "2023-05-05T21:57:24.295Z",
-    updatedAt: "2023-05-05T21:57:24.295Z",
-    exam_id: 1,
-    exam: {
-      id: 1,
-      exam_name: "Java Exam",
-      duration_minutes: 120,
-      exam_start: "2023-04-12T20:26:44.755Z",
-      exam_end: "2023-04-12T20:26:44.755Z",
-      createdAt: "2023-05-05T15:29:15.540Z",
-      updatedAt: "2023-05-05T15:29:15.540Z",
-      course_id: null,
-    },
-    answers: [],
-  },
-];
